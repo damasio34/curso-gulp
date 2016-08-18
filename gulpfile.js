c2c5bcc971a87a86f1df7fe972293a3ad8dd55d4
Darlan Damasio <damasio34@gmail.com>
2016-08-18 19:19:04 -0300

    Aula 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,21 @@
 var gulp = require('gulp');
-imagemin = require('gulp-imagemin');
+var imagemin = require('gulp-imagemin');
+var clean = require('gulp-clean');
 
-gulp.task('copy', function() {
-    gulp.src('src/**/*')
+gulp.task('copy', ['clean'], function() {
+    return gulp.src('src/**/*')
         .pipe(gulp.dest('dist'));
-})
+});
 
-gulp.task('build-img', function() {
-    gulp.src('src/img/**/*')
+gulp.task('clean', function() {
+    return gulp.src('dist')
+        .pipe(clean());
+});
+
+gulp.task('build-img', ['copy'], function() {
+    return gulp.src('dist/img/**/*')
         .pipe(imagemin())
         .pipe(gulp.dest('dist/img/'));
 });
+
+gulp.task('default', ['build-img']);
