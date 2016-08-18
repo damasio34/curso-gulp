5c1e8be84b3f9868801ed7b7804b0a2937beac7a
Darlan Damasio <damasio34@gmail.com>
2016-08-18 18:52:20 -0300

    Aula 1 e parte da Aula 2

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+imagemin = require('gulp-imagemin');
+
+gulp.task('copy', function() {
+    gulp.src('src/**/*')
+        .pipe(gulp.dest('dist'));
+})
+
+gulp.task('build-img', function() {
+    gulp.src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img/'));
+});
